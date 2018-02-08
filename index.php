a61878fda4691023d175cd24528c98fabd08a775
DevinBurbach1432 <500152227@fvtc.edu>
2018-02-07 20:26:54 -0600

    test

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-18">
+    <title>Devin's Homepage</title>
+    <link rel="stylesheet" type="text/css" href="css/base.css">
+</head>
+<body>
+    <header><h1>Devin's Website</h1></header>
+    <nav>
+        <ul>
+            <li><A href="/">Homepage</A> </li>
+            <li><A href="#">Loop</A> </li>
+            <li><A href="#">Countdown</A> </li>
+        </ul>
+    </nav>
+    <main>
+        <img src="img/devin.png" alt="picture of devin">
+        <p>efrigu nlaiegru ldfjnzldkfjbgaoli grub lknfv laeirugblzdkfjv leiarblzdkjfgb lriguzldkfng lerjgzdkfnv lerildj gleir lglzdkfng liejrg lkdnfvldkxfjbg lfvzldkfnv alerjvldkfnv ldfjv ldkjfvdlfj dlfkjv ldfkj lkdjv ldzkjfvn,skjrvblifbv lzdkfvldfkjvbz lkjdfbv lkdjfvb ldv b</p>
+    </main>
+    <footer>&copy; 20xx DevinBurbach.com</footer>
+</body>
+
+</html>
